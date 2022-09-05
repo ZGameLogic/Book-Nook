30ac2706a1aca5441af4c5add78adaa32b520239
Ben Shabowski <benshabowski@gmail.com>
2022-09-05 11:55:09 -0500

    Initial Commit

---
+++ Book Nook/Book_NookApp.swift
@@ -0,0 +1,20 @@
+//
+//  Book_NookApp.swift
+//  Book Nook
+//
+//  Created by Benjamin Shabowski on 9/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Book_NookApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
